914c994021d38539d78723ac87afce05b3f29482
Cesar Bernal <cesarbworkmail@gmail.com>
2019-02-16 17:02:08 +0000

    mistake

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-cesar-cesarabcwork.c9users.io','blog-test-app-cesar.heroku.com']
+ALLOWED_HOSTS = ['django-blog-cesar-cesarabcwork.c9users.io','blog-test-app-cesar.herokuapp.com']
 
 
 # Application definition
